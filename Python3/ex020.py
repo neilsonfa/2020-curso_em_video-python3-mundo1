b8e3874e7f3d3ffa6c8c3410f1a3111058f021d8
Neilson Andrade <neilsonfa@gmail.com>
2020-05-01 22:30:15 -0300

    Inclusão de novos exercícios
    Inclusão dos exercícios 19  e 20, mas a preparação para o 21.

--- Python3/ex020.py
+++ Python3/ex020.py
@@ -1,8 +1,8 @@
-from random import choice
+from random import shuffle
 aluno1 = input('Digite o nome do primeiro aluno:\n')
 aluno2 = input('Digite o nome do segundo aluno:\n')
 aluno3 = input('Digite o nome do terceiro aluno:\n')
 aluno4 = input('Digite o nome do quarto aluno:\n')
 alunos = [aluno1, aluno2, aluno3, aluno4]
-sorteado = choice(alunos)
-print('{} apagará o quadro.'.format(sorteado))
+shuffle(alunos)
+print('{} apagará o quadro.'.format(alunos))
--- Python3/test.py
+++
@@ -1,3 +0,0 @@
-from math import sqrt
-a = int(input('Teste:'))
-print('O resultado do teste é {}.'.format(sqrt(a)))
