b4291f7c333833042d1c2c83e78a165be8ba7732
Neilson Andrade <neilsonfa@gmail.com>
2020-05-11 21:25:24 -0300

    O exercício 23 foi concluído. Tentei corrigir o exercício 21, onde não está encerrando o programe após a reprodução do audio.

--- Python3/ex021.py
+++ Python3/ex021.py
@@ -1,4 +1,5 @@
 import pygame
+
 pygame.mixer.init()
 pygame.init()
 pygame.mixer.music.load('ex021.mp3')
--- Python3/ex023.py
+++ Python3/ex023.py
@@ -1,1 +1,10 @@
-print('Testando comoando do Git pelo Git Bash!')
+numero = int(input('Digite um número de 0 a 9999: '))
+unidade = numero // 1 % 10
+dezena = numero // 10 % 10
+centena = numero // 100 % 10
+milhar = numero // 1000 % 10
+
+print('A unidade é {}.'.format(unidade))
+print('A dezena é {}.'.format(dezena))
+print('A centena é {}.'.format(centena))
+print('O milhar é {}.'.format(milhar))
